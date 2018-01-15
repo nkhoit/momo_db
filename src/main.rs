2ae82aa423730c3b21003af0264708a8af463506
km42 <kurtmueller42@gmail.com>
2018-01-14 23:26:20 -0500

    Working on adding real database interactions

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,61 @@ extern crate postgres;
 
 use postgres::{Connection, TlsMode};
 
-// TODO remove
-#[get("/<name>/<age>")]
-fn hello(name: String, age: u8) -> String {
-    format!("Hello, {} year old named {}!", age, name)
+struct Identity {
+    id: i64,
+    balance: f64,
+    d_id: Option<i64>, //discord id if the identity has one
+    pkey: Option<String> //public key if the identity has one
+
 }
+
+/**
+ * IdentityService. TODO: refactor to another file
+ */
+
+// Loads identity associated with the discord id. If none exists, create one.
+fn load_from_did(d_id: i64, conn: Connection) -> Identity {
+  let rows = &conn.query("select * from wlt_id cross join discord_user where wlt_id.id = discord_user.wlt_id", &[&d_id]).unwrap();
+  let mut id: i64 = 0;
+  let mut balance: f64 = 0.0;
+  let mut pkey: Option<String> = None;
+  if rows.len() == 0 {
+      println!("Creating new identity for discord id {}", d_id);
+    //  &conn.query("insert into wlt_id (momo_bal) values (0.0) returning id");
+  } else {
+      let row = rows.get(0);
+      id = row.get(0);
+      balance = row.get(1);
+
+      // Check for the pkey
+      for row in &conn.query("select pubkey from pubkey_user where wlt_id=$1", &[&id]).unwrap(){
+          pkey = row.get(0);
+      }
+  }
+  Identity{
+      id : id,
+      balance: balance,
+      d_id: Some(d_id),
+      pkey: pkey
+  }
+}
+
+/**
+ * End of IdentityService
+ */
 
 // get balance by discord id
 #[get("/discord/<id>")]
-fn balance_by_id(id: u64) -> String {
+fn balance_by_id(id: i64) -> String {
     let conn = Connection::connect("postgres://postgres:test@localhost:5432/momo", TlsMode::None).unwrap();
-    for row in &conn.query("SELECT momo_bal FROM wlt_id WHERE id=1", &[]).unwrap() {
-        let balance : f64 = row.get("momo_bal");
+    let mut balance = 0.0;
+    let mut found : bool = false;
+    for row in &conn.query("SELECT momo_bal FROM wlt_id WHERE id=$1", &[&id]).unwrap() {
+        balance = row.get("momo_bal");
+        found = true;
         println!("Queried and got {}", balance);
     }
-    format!("{}", 0.0) // TODO
+    format!("{}", balance)
 }
 
 // get balance by stellar public key
@@ -31,7 +71,6 @@ fn balance_by_key(pkey: String) -> String {
 
 
 fn main() {
-    rocket::ignite().mount("/hello", routes![hello])
-                    .mount("/wallet", routes![balance_by_id, balance_by_key])
+    rocket::ignite().mount("/wallet", routes![balance_by_id, balance_by_key])
                     .launch();
 }
